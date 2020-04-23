d7b68efbc36c993ac568bc7030c69d531b2e7392
Artem Bakhanov <artembakhanov@gmail.com>
2020-04-23 22:07:16 +0300

    Change models

--- back/admin.py
+++ back/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import User, Hotel
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('telegram_id', 'first_name', 'last_name')
+    list_display = ('id', 'telegram_id', 'first_name', 'last_name')
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'admin_telegram_id', 'website')
