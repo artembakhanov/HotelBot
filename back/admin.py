5773b332764861a7170c9b325df2c5e1d3371952
Artem Bakhanov <artembakhanov@gmail.com>
2020-04-25 16:40:35 +0300

    Add all models

--- back/admin.py
+++ back/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Hotel
+from .models import User, Hotel, RoomCategory, Room, Booking
 
 
 @admin.register(User)
@@ -12,3 +12,18 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Hotel)
 class HotelAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'admin_telegram_id', 'website')
+
+
+@admin.register(RoomCategory)
+class RoomCategoryAdmin(admin.ModelAdmin):
+    list_display = ("hotel", "name", "min_price")
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("room_category", "name")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("room", "confirmation_number", "active")
--- back/models.py
+++ back/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -13,3 +14,32 @@ class Hotel(models.Model):
     name = models.TextField()
     admin_telegram_id = models.IntegerField()
     website = models.URLField()
+
+    def __str__(self):
+        return f"Hotel \"{self.name}\""
+
+
+class HotelInfo(models.Model):
+    button_name = models.TextField()
+    content = models.TextField()
+
+
+class RoomCategory(models.Model):
+    class Meta:
+        verbose_name_plural = "Room categories"
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    name = models.TextField()
+    min_price = models.PositiveIntegerField()
+
+
+class Room(models.Model):
+    room_category = models.ForeignKey(RoomCategory, on_delete=models.PROTECT)
+    name = models.TextField()
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.PROTECT)
+    confirmation_number = models.TextField(validators=[RegexValidator("[0-9]{8}")])
+    date_check_in = models.DateTimeField()
+    date_check_out = models.DateTimeField()
+    active = models.BooleanField(default=False, auto_created=True)
