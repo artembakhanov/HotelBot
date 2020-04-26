7381bc2fcb32f15b0c9227b695101e7ec9cfb749
Artem Bakhanov <artembakhanov@gmail.com>
2020-04-26 16:55:40 +0300

    Add feedback support

--- back/management/commands/bot/main.py
+++ back/management/commands/bot/main.py
@@ -3,7 +3,7 @@ import re
 import telebot
 
 from HotelManagementBot import settings
-from back.models import User as User_, Booking
+from back.models import User as User_, Hotel, Question
 from .static import *
 from .wrappers import save_user
 
@@ -11,7 +11,9 @@ HOTEL_ID = 1  # this was asked to be const
 
 
 def main():
-    bot = telebot.TeleBot(settings.TOKEN, )
+    bot = telebot.TeleBot(settings.TOKEN)
+
+    question_states = {}
 
     @bot.message_handler(commands=['start'])
     @save_user
@@ -23,10 +25,18 @@ def main():
 
     @bot.message_handler(content_types=['text'], func=lambda m: m.text == MY_BOOKINGS)
     @save_user
-    def hotel_info(m, user):
+    def see_booking(m, user):
         bot.send_message(m.chat.id, "Please, enter your booking id and confirmation code.")
         user.state = State.BOOKING_CODE.value
         user.save()
+
+    @bot.message_handler(content_types=['text'], func=lambda m: m.text == HOTEL_INFO)
+    @save_user
+    def hotel_info(m, user):
+        hotel = Hotel.objects.get(id=HOTEL_ID)
+        bot.send_message(m.chat.id,
+                         f"*{hotel.name}*\n\n",
+                         reply_markup=HOTEL_BUTTONS(hotel))
 
     @bot.message_handler(content_types=['text'],
                          func=lambda m: User_.objects.get(telegram_id=m.from_user.id).state == State.BOOKING_CODE.value)
@@ -35,9 +45,6 @@ def main():
         match = re.fullmatch(r'([0-9]+)\s+([0-9]{8})', m.text)
         if match:
             booking_id, confirmation_code = match.groups()
-            user.state = State.DEFAULT.value
-            user.save()
-
             try:
                 booking = Booking.objects.get(id=booking_id, confirmation_number=confirmation_code)
                 sym = "" if booking.active else "~"
@@ -49,12 +56,98 @@ def main():
                     parse_mode="MarkdownV2",
                     reply_markup=BOOKING_BUTTONS(booking.id)
                 )
+                user.state = State.DEFAULT.value
+                user.save()
             except Booking.DoesNotExist:
                 bot.send_message(m.chat.id, "The booking with such id and the confirmation code does not exist.\n"
                                             "Please try again.")
-            # send booking here
         else:
             bot.send_message(m.chat.id,
                              "The message has incorrect format. Firstly, put booking id and then confirmation code.")
+
+    @bot.message_handler(content_types=['text'],
+                         func=lambda m: User_.objects.get(
+                             telegram_id=m.from_user.id).state == State.QUESTION_ANSWER.value)
+    @save_user
+    def send_question(m, user):
+        user.state = State.DEFAULT.value
+        user.save()
+
+        text = m.text
+        q = question_states[m.from_user.id]
+        if q['booking_id'] is None:
+            booking = None
+        else:
+            booking = Booking.objects.get(id=q['booking_id'])
+
+        question = Question(user=user, booking=booking, content=text)
+        question.save()
+        message = bot.send_message(q["admin"],
+                                   f"⚠️ New Question \\#{question.id}\n"
+                                   f"\\[Booking \\#{q['booking_id']}\\]\n\n"
+                                   f"_{text}_\n\n"
+                                   f"*Please reply to this message with your answer*\\.", parse_mode="MarkdownV2")
+
+        question.message_id = message.message_id
+        question.save()
+
+        del q
+
+        bot.send_message(m.chat.id, f"✅ Your question #{question.id} is sent to the admin.\n"
+                                    f" You will get the answer soon.",
+                         reply_markup=MAIN_MENU)
+
+    @bot.message_handler(content_types=['text'],
+                         func=lambda m: m.reply_to_message and Question.objects.get(
+                             message_id=m.reply_to_message.message_id))
+    def write_answer(m):
+        answer = m.text
+
+        question = Question.objects.get(message_id=m.reply_to_message.message_id)
+        question.answer = answer
+        question.save()
+
+        booking_text = f"regarding your booking *\\#{question.booking.id}*" if question.booking else ""
+        bot.send_message(question.user.telegram_id,
+                         f"✅ You got an answer to question *\\#{question.id}* {booking_text}\n\n"
+                         f"_{answer}_",
+                         parse_mode="MarkdownV2")
+
+    @bot.callback_query_handler(func=lambda call: call.data[:2] == "h_")
+    def write_message_booking(call):
+        booking_id = call.data[2:]
+        if booking_id == "":
+            booking_id = None
+        admin = Hotel.objects.get(id=HOTEL_ID).admin_telegram_id
+
+        question_states[call.from_user.id] = {"booking_id": booking_id, "admin": admin}
+
+        print(call.from_user.id)
+        user = User_.objects.get(telegram_id=call.from_user.id)
+
+        user.state = State.QUESTION_ANSWER.value
+        user.save()
+
+        bot.send_message(call.message.chat.id, f"Please, state your question in the next message.",
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+
+    @bot.callback_query_handler(func=lambda call: call.data[:2] == "c_")
+    def write_message_booking(call):
+        booking_id = call.data[2:]
+        try:
+            booking = Booking.objects.get(id=booking_id)
+            booking.active = False
+            booking.save()
+            sym = "" if booking.active else "~"
+            bot.edit_message_text(f"📖 {sym}Booking{sym} *\\#{booking.id}*\n\n"
+                                  f"🛏 Room: {booking.room.name}\n"
+                                  f"{'✅ The booking is active' if booking.active else '❌ The booking is cancelled'}",
+                                  parse_mode="MarkdownV2",
+                                  reply_markup=BOOKING_BUTTONS(booking.id),
+                                  message_id=call.message.message_id,
+                                  chat_id=call.message.chat.id)
+            bot.answer_callback_query(call.id, "Cancelled!")
+        except Booking.DoesNotExist:
+            pass
 
     bot.polling()
--- back/management/commands/bot/static.py
+++ back/management/commands/bot/static.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from telebot.types import *
 
+from back.models import Booking
+
 HOTEL_INFO = "ℹ️ Hotel Info"
 MY_BOOKINGS = "📖 My Bookings"
 
@@ -12,10 +14,24 @@ MAIN_MENU.row(HOTEL_INFO, MY_BOOKINGS)
 def BOOKING_BUTTONS(booking_id):
     keyboard = InlineKeyboardMarkup()
     booking_message_button = InlineKeyboardButton("ℹ️ Contact us", callback_data=f"h_{booking_id}")
+    cancel_booking_button = InlineKeyboardButton("❌ Cancel", callback_data=f"c_{booking_id}")
     keyboard.add(booking_message_button)
+    if Booking.objects.get(id=booking_id).active:
+        keyboard.add(cancel_booking_button)
+    return keyboard
+
+
+def HOTEL_BUTTONS(hotel):
+    keyboard = InlineKeyboardMarkup()
+    url_button = InlineKeyboardButton("WebSite", url=hotel.website)
+    hotel_message_button = InlineKeyboardButton("ℹ️ Contact us", callback_data=f"h_")
+    keyboard.add(url_button)
+    keyboard.add(hotel_message_button)
+
     return keyboard
 
 
 class State(Enum):
     DEFAULT = 0
     BOOKING_CODE = 1
+    QUESTION_ANSWER = 2
